0b57cdd423284c32c185be996e65e13b2799c1c6
mahtoid <56257224+mahtoid@users.noreply.github.com>
2020-11-22 12:49:01 +0000

    Versioning Update to sync with PyPi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as fh:
 
 setup(
     name="chat_exporter",
-    version="1.1.5",
+    version="1.1.5.5",
     author="mahtoid",
     description="A simple Discord chat exporter for Python Discord bots.",
     long_description=long_description,
