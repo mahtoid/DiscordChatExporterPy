1907ae5b8296f1a4e29d6b0c3a63912a910a2152
mahtoid <matthewmoss98@gmail.com>
2021-01-29 14:55:50 +0000

    Fix References: when origin message is deleted

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as fh:
 
 setup(
     name="chat_exporter",
-    version="1.3.0.3",
+    version="1.3.0.4",
     author="mahtoid",
     description="A simple Discord chat exporter for Python Discord bots.",
     long_description=long_description,
