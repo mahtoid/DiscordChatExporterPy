95b982ac1fc5e62e623d1bbcab766d4189679daa
mahtoid <matthewmoss98@gmail.com>
2021-07-28 20:12:21 +0100

    Versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as fh:
 
 setup(
     name="chat_exporter",
-    version="1.6.1",
+    version="1.6.2",
     author="mahtoid",
     description="A simple Discord chat exporter for Python Discord bots.",
     long_description=long_description,
